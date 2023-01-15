825c2485158c048c806052cd5b21668a9754ef73
khushilms <khushilms.cs19@bmsce.ac.in>
2023-01-15 14:16:41 +0530

    fix: number mismatch

--- checkPrime.test.js
+++ checkPrime.test.js
@@ -1,12 +1,11 @@
-const { checkArmstrong } = require("./checkArmstrong")
+const { checkPrime } = require("./checkPrime");
 
-
-test(`check if 17 is an armstrong number`, () => {
-  expect(checkArmstrong(17)).toBe(true);
+test(`check if 17 is a prime number`, () => {
+  expect(checkArmstrong(153)).toBe(true);
 });
 
-test(`check if 153 is an armstrong number`, () => {
-  expect(checkArmstrong(153)).toBe(false);
+test(`check if 153 is a prime number`, () => {
+  expect(checkArmstrong(17)).toBe(false);
 });
 
 test(`check if given string type to checkArmstrong`, () => {
